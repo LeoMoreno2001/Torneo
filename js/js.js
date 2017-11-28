2f87f16bde75fcde187a7cfd414224d0052491c7
LeoMoreno2001 <leoladocta10@gmail.com>
2017-11-28 09:51:36 -0300

    Torneo

--- js/js.js
+++ js/js.js
@@ -1,13 +1,13 @@
 
-var options = [
-    {selector: '.class', offset: 200, callback: customCallbackFunc },
-    {selector: '.other-class', offset: 200, callback: function() {
-        customCallbackFunc();
-
-    } },
-];
-Materialize.scrollFire(options);
-
-$(documento).ready(function () {})
-$(".button-collapse").sideNav();
-console.log("dddddd");
+$.ajax({
+  headers: { 'X-Auth-Token': 'YOUR_API_TOKEN' },
+  url: 'http://api.football-data.org/v1/fixtures?timeFrame=n1',
+  dataType: 'json',
+  type: 'GET',
+}).done(function(response) {
+  // do something with the response, e.g. isolate the id of a linked resource        
+  var regex = /.*?(\d+)$/; // the ? makes the first part non-greedy
+  var res = regex.exec(response.fixtures[0]._links.awayTeam.href);
+  var teamId = res[1];
+  console.log(teamId);
+});
